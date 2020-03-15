b8be34be497fdfccdbd83cf8e262cbba0314e6ae
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-03-15 17:56:02 +0100

    Maak visualiser af

--- js/d3.js
+++ js/d3.js
@@ -13,18 +13,12 @@ d3.json("https://apti.be/discord/api")
     const baseMultiplier = 10;
 
     const firstField = data[0];
-    const height = firstField.aantal * baseMultiplier * translateMultiplier *1.5;
+    const height = firstField.aantal * baseMultiplier * translateMultiplier * 1.5;
     const width = dataLength * baseMultiplier * translateMultiplier;
-
-    console.log(dataLength)
-    console.log(firstField);
-    console.log(firstField._id);
 
     var dateFromObjectId = function (objectId) {
       return new Date(parseInt(objectId.substring(0, 8), 16) * 1000).toLocaleString();
     };
-
-    console.log(dateFromObjectId(firstField._id));
 
     const svg = d3
       .select("#data-here")
@@ -41,7 +35,7 @@ d3.json("https://apti.be/discord/api")
         return idx * baseMultiplier;
       })
       .attr("y", d => {
-        return height - d.aantal * baseMultiplier - 200;
+        return height - d.aantal * baseMultiplier - 120;
       })
       .attr("height", d => {
         const x = d.aantal * baseMultiplier;
@@ -53,32 +47,43 @@ d3.json("https://apti.be/discord/api")
       .attr("width", () => {
         return baseMultiplier;
       })
-      .attr("transform", (d, idx) => {
-        return `translate(${[translateMultiplier * idx, 0]})`
-      })
       .attr("fill", d => {
         if (d.aantal > 50) return "rgb(204,0,0)";
         else if (d.aantal > 40) return "rgb(204,102,0)";
         else if (d.aantal > 30) return "rgb(204,204,0)";
         else if (d.aantal > 20) return "rgb(102,204,0)";
         else if (d.aantal > 10) return "rgb(0,0,204)";
+        else if (d.aantal > 5) return "rgb(0,102,102)";
         else return "rgb(0,0,0";
       });
 
-      const text = svg.selectAll("text").data(data);
-
-      text.enter()
+    const text = svg.selectAll("text").data(data);
+    
+    text.enter()
       .append("text")
       .attr("x", (d, idx) => {
-        return idx * baseMultiplier;
+        return 0;
       })
       .attr("y", d => {
-        return height - d.aantal * baseMultiplier;
+        return 0;
       })
-      .text(d => { return dateFromObjectId(d._id);})
+      .text(d => { return dateFromObjectId(d._id); })
       .attr("font-family", "sans-serif")
-      .attr("font-size", "20px")
-      .attr("fill", "red");
+      .attr("font-size", "12px")
+      .attr("fill", d => {
+        if (d.aantal > 55) return "rgb(204,0,0)";
+        else if (d.aantal > 45) return "rgb(204,102,0)";
+        else if (d.aantal > 35) return "rgb(204,204,0)";
+        else if (d.aantal > 25) return "rgb(102,204,0)";
+        else if (d.aantal > 15) return "rgb(0,0,204)";
+        else if (d.aantal > 5) return "rgb(0,102,102)";
+        else return "rgb(0,0,0";
+      })
+      .attr('transform', (d, idx) => {
+        const x = idx * baseMultiplier + baseMultiplier;
+        const y = height;
+        return 'translate( ' + x + ' , ' + y + '),' + 'rotate(-90)';
+      });
   })
   .catch(error => {
     console.log(error);
