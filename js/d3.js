9e950865cfafe4fde1f0cb836dfabfa6f24ffaff
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-03-15 17:28:52 +0100

    Add text

--- js/d3.js
+++ js/d3.js
@@ -1,11 +1,22 @@
 d3.json("https://apti.be/discord/api")
   .then(data => {
+    var newData = [];
+
+    for (let i = 0; i < data.length; i += 60) {
+      newData.push(data[i]);
+    }
+
+    data = [...newData];
+
+    const dataLength = data.length;
+    const translateMultiplier = 2;
+    const baseMultiplier = 10;
 
     const firstField = data[0];
-    const height = firstField.aantal * 25;
-    const width = data.length;
-    const multi = 4;
+    const height = firstField.aantal * baseMultiplier * translateMultiplier *1.5;
+    const width = dataLength * baseMultiplier * translateMultiplier;
 
+    console.log(dataLength)
     console.log(firstField);
     console.log(firstField._id);
 
@@ -23,34 +34,28 @@ d3.json("https://apti.be/discord/api")
 
     const rectangles = svg.selectAll("rect").data(data);
 
-    svg.append("line")
-    .attr("x", (d) =>{
-      return 0;
-    })
-    .attr("y", (d) =>{
-      return 0;
-    })
-    .attr("height", (d) =>{
-      return 200;
-    });
-
     rectangles
       .enter()
       .append("rect")
       .attr("x", (d, idx) => {
-        return idx;
+        return idx * baseMultiplier;
       })
-      .attr("y", function (d, idx) {
-        return height - d.aantal * multi;
+      .attr("y", d => {
+        return height - d.aantal * baseMultiplier - 200;
       })
-      .attr("height", function (d) {
-        const x = d.aantal * multi;
+      .attr("height", d => {
+        const x = d.aantal * baseMultiplier;
 
         if (x <= 0) return 0;
 
         return x;
       })
-      .attr("width", "1px")
+      .attr("width", () => {
+        return baseMultiplier;
+      })
+      .attr("transform", (d, idx) => {
+        return `translate(${[translateMultiplier * idx, 0]})`
+      })
       .attr("fill", d => {
         if (d.aantal > 50) return "rgb(204,0,0)";
         else if (d.aantal > 40) return "rgb(204,102,0)";
@@ -59,6 +64,21 @@ d3.json("https://apti.be/discord/api")
         else if (d.aantal > 10) return "rgb(0,0,204)";
         else return "rgb(0,0,0";
       });
+
+      const text = svg.selectAll("text").data(data);
+
+      text.enter()
+      .append("text")
+      .attr("x", (d, idx) => {
+        return idx * baseMultiplier;
+      })
+      .attr("y", d => {
+        return height - d.aantal * baseMultiplier;
+      })
+      .text(d => { return dateFromObjectId(d._id);})
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "20px")
+      .attr("fill", "red");
   })
   .catch(error => {
     console.log(error);
