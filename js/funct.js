468c065f0c0b759adac52e298794e7b6fddd8749
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-03-18 11:29:22 +0100

    Update funct.js

--- js/funct.js
+++ js/funct.js
@@ -11,20 +11,24 @@ CalculateMax = function (d) {
     let max = 0;
     for (const t in d) {
         let temp = d[t].aantal;
-        if (temp > max) max= temp;
+        if (temp > max) max = temp;
     }
 
     return max;
 };
 
 ColorCode = function (cnt) {
-    if (cnt > 50) return "rgb(204,0,0)";
-    else if (cnt > 40) return "rgb(204,102,0)";
-    else if (cnt > 30) return "rgb(204,204,0)";
-    else if (cnt > 20) return "rgb(102,204,0)";
-    else if (cnt > 10) return "rgb(0,0,204)";
-    else if (cnt > 5) return "rgb(0,102,102)";
-    else return "rgb(0,0,0";
+    // if (cnt > 50) return "rgb(204,0,0)";
+    // else if (cnt > 40) return "rgb(204,102,0)";
+    // else if (cnt > 30) return "rgb(204,204,0)";
+    // else if (cnt > 20) return "rgb(102,204,0)";
+    // else if (cnt > 10) return "rgb(0,0,204)";
+    // else if (cnt > 5) return "rgb(0,102,102)";
+    // else return "rgb(0,0,0";
+    let r = Math.floor(255 / cnt);
+    let g = Math.floor(255 / cnt);
+    let b = Math.floor(255 / cnt);
+    return "rgb(" + r + ", " + g + ", " + b + ") ";
 };
 
 DateFromObjectId = function (oID) {
