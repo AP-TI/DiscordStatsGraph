8b8c9e96d066e515d5597e6d4945f5b7e4be2864
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-03-18 11:51:22 +0100

    Restore

--- js/funct.js
+++ js/funct.js
@@ -18,17 +18,17 @@ CalculateMax = function (d) {
 };
 
 ColorCode = function (cnt) {
-    // if (cnt > 50) return "rgb(204,0,0)";
-    // else if (cnt > 40) return "rgb(204,102,0)";
-    // else if (cnt > 30) return "rgb(204,204,0)";
-    // else if (cnt > 20) return "rgb(102,204,0)";
-    // else if (cnt > 10) return "rgb(0,0,204)";
-    // else if (cnt > 5) return "rgb(0,102,102)";
-    // else return "rgb(0,0,0";
-    let r = Math.floor(255 / cnt * Math.random() * 10);
-    let g = Math.floor(255 / cnt * Math.random() * 10);
-    let b = Math.floor(255 / cnt * Math.random() * 10);
-    return "rgb(" + r + ", " + g + ", " + b + ") ";
+    if (cnt > 50) return "rgb(204,0,0)";
+    else if (cnt > 40) return "rgb(204,102,0)";
+    else if (cnt > 30) return "rgb(204,204,0)";
+    else if (cnt > 20) return "rgb(102,204,0)";
+    else if (cnt > 10) return "rgb(0,0,204)";
+    else if (cnt > 5) return "rgb(0,102,102)";
+    else return "rgb(0,0,0";
+    // let r = Math.floor(255 / cnt * Math.random() * 10);
+    // let g = Math.floor(255 / cnt * Math.random() * 10);
+    // let b = Math.floor(255 / cnt * Math.random() * 10);
+    // return "rgb(" + r + ", " + g + ", " + b + ") ";
 };
 
 DateFromObjectId = function (oID) {
